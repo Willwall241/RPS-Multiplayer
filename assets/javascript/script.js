95d4dd45be3b5e89b21e8e3d440efdd735e6c6b7
WJW <will.j.waller@gmail.com>
2018-06-30 22:45:46 -0400

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,17 +10,23 @@ var config = {
 };
 firebase.initializeApp(config);
 
-//load page with player data or submit name to play or waiting for other player
+//Load page with player data or submit name to play or waiting for other player
 var database = firebase.database();
 
 var player = 0;
 var otherPlayer = 0;
 var playersReady = false;
+var numberOfPlayers = 0;
 
-
+//Function to create buttons
 function addButtons() {
 
-  var buttonNames = ["Rock", "Paper", "Scissors"];
+  //Names of buttons
+  var buttonNames = ["rock", "paper", "scissors"];
+  $("#p1-buttons").empty();
+  $("#p2-buttons").empty();
+
+  //Loop through buttonNames array to create butons
   for (i = 0; i < buttonNames.length; i++) {
     var newButton = $("<button>");
     newButton.text(buttonNames[i]);
@@ -32,13 +38,88 @@ function addButtons() {
   }
 }
 
+console.log(playersReady);
+//Function to check winner
+function checkWinner(player2Choice) {
+  firebase.database().ref().once("value").then(function (snapshot) {
+    // On tie
+    if (snapshot.child("player/1/choice").val() == player2Choice) {
+      // Set winner to 0 (tie game)
+      database.ref("winner").set("0");
 
+    } // On player 1 win
+    else if ((snapshot.child("player/1/choice").val() == "rock" && snapshot.child("player/2/choice").val() == "scissors") ||
+      (snapshot.child("player/1/choice").val() == "paper" && snapshot.child("player/2/choice").val() == "rock") ||
+      (snapshot.child("player/1/choice").val() == "scissors" && snapshot.child("player/2/choice").val() == "paper")) {
 
-$(function() {
+      // Add to player 1 wins and player 2 loses
+      firebase.database().ref("player/1/wins").transaction(function (currentWins) { return currentWins + 1; });
+      firebase.database().ref("player/2/loses").transaction(function (currentLoses) { return currentLoses + 1; });
+
+      // Set winner to 1 (player 1)
+      firebase.database().ref("winner").set("1");
+
+    } // On player 2 win
+    else {
+      // Add to player 2 wins and player 1 loses
+      firebase.database().ref("player/2/wins").transaction(function (currentWins) { return currentWins + 1; });
+      firebase.database().ref("player/1/loses").transaction(function (currentLoses) { return currentLoses + 1; });
+
+      // Set winner to 2 (player 2)
+      firebase.database().ref("winner").set("2");
+    }
+  });
+}
+
+//Function to show winner on page
+function showWinner() {
+  firebase.database().ref().once("value").then(function (snapshot) {
+    // Show text on page depending on win/loss/tie
+    switch (snapshot.child("winner").val()) {
+      case "0": $("#outcome-stage").html("<h2>Its a tie!<h2>"); break;
+      case "1": $("#outcome-stage").html("<h2>" + snapshot.child("player/1/name").val() + " wins!</h2>"); break;
+      case "2": $("#outcome-stage").html("<h2>" + snapshot.child("player/2/name").val() + " wins!</h2>"); 
+    }
+
+    // // Remove previous game data if still present
+    // if (snapshot.child("1/choice").exists()) { firebase.database().ref("player/1/choice").remove(); }
+    // if (snapshot.child("winner").exists()) { firebase.database().ref("win/winner").remove(); }
+
+    setTimeout(resetGame, 2000);
+
+    // Start game over
+    function resetGame() {
+      $("#outcome-stage").html("");
+      database.ref("player/1/choice").remove();
+      database.ref("player/2/choice").remove();
+      database.ref("winner").remove();
+      $("#outcome-stage").html("Waiting for Player 1 choice!");
+      addButtons();
+    }
+  });
+}
+
+//Function to listens for a child being removed to take players off page
+database.ref("player").on("child_removed", function (snapshot) {
+
+  if (snapshot.val().name) {
+    $("#p" + otherPlayer + "-name").text("Waiting for player");
+    $("#p" + otherPlayer + "-wins").empty();
+    $("#p" + otherPlayer + "-loses").empty();
+    $("#p" + otherPlayer + "-buttons").empty();
+
+    console.log("hi");
+  }
+
+});
+
+//Document ready functoin
+$(function () {
   //On click event listener for start button
   $("#start-button").on("click", function () {
     event.preventDefault();
 
+    //Function that listens to value change in Firebase
     database.ref("player").once("value").then(function (snapshot) {
 
       //Set player input to name
@@ -49,40 +130,34 @@ $(function() {
       //Check if player 1 exists in firebase if not create player
       if (!snapshot.child("1").exists()) {
         player = 1;
+        otherPlayer = 2;
+        numberOfPlayers++;
         console.log("blah");
+        console.log(numberOfPlayers);
 
+        //Create player child in Firebase
         database.ref("player/1").set({
           name: name,
           wins: 0,
           loses: 0
         });
 
-        nameDisplay.text(name);
-        nameDisplay.addClass("p1-name");
-        // $("#p1-stage").append(nameDisplay)
-
-        otherPlayer = 2;
-        // addButtons
-        console.log("player" + player);
 
       }
       //Check if player 2 exists in firebase if not create player
       else if (!snapshot.child("2").exists()) {
         player = 2;
-        console.log("naw");
+        otherPlayer = 1;
+        numberOfPlayers++;
 
+        //Create player child in Firebase
         database.ref("player/2").set({
           name: name,
           wins: 0,
           loses: 0
         });
 
-        nameDisplay.text(name);
-        nameDisplay.addClass("p2-name");
-        // $("#p2-stage").append(nameDisplay)
 
-        otherPlayer = 1;
-        console.log("player" + player);
       }
       //if player 1 and player 2 exists display message in outcome-stage div
       else {
@@ -91,68 +166,113 @@ $(function() {
         $("#outcome-stage").append(nameDisplay)
       }
 
-
-
+      
     });
 
-
-
   });
 
-  $(document).on('click', '.player1-choice', function(){ 
-    console.log("test");
-  
-    database.ref("player/1/choice").set($(this).attr("data"));
-    $(".player1-choice").remove();
-    $("#p1-buttons").append($(this).attr("data"));
+
+  //Click listener for player one buttons
+  $(document).on('click', '.player1-choice', function () {
+
+
+    database.ref("player/" + player + "/choice").set($(this).attr("data"));
+    $(".player-choice").remove();
+    $("#p" + player + "-buttons").append($(this).attr("data"));
     console.log($(this).attr("data"));
-  
+
+    database.ref("player/" + player).update({
+      choice: $(this).attr("data")
+    });
+
   });
-  
-  $(document).on('click', '.player2-choice', function(){  
-  
-    database.ref("player/2/choice").set($(this).attr("data"));
-    $(".player2-choice").remove();
-    $("#p2-buttons").append($(this).attr("data"));
+
+  //Click listener for player two buttons
+  $(document).on('click', '.player2-choice', function () {
+
+
+    database.ref("player/" + player + "/choice").set($(this).attr("data"));
+    $(".player-choice").remove();
+    $("#p" + player + "-buttons").append($(this).attr("data"));
     console.log($(this).attr("data"));
-  
-  });
 
+    database.ref("player/" + player).update({
+      choice: $(this).attr("data")
+    });
 
-  $(window).on("beforeunload", function () {
-    database.ref("player/" + player).remove();
-  });
+    checkWinner();
+
 });
 
-database.ref("player").on("value", function (snapshot) {
 
+//Unload listener to remove information from Firebase on refresh or closing page
+$(window).on("beforeunload", function () {
+  database.ref("player/" + player).remove();
+  database.ref("winner").remove();
+  playersReady = false;
 
-  //fill page with content when players exist
-  if (snapshot.child("1").exists()) {
+});
+});
 
-    $("#p1-name").html(snapshot.child("1/name").val());
-    $("#p1-wins").html("Wins: " + snapshot.child("1/wins").val());
-    $("#p1-loses").html("Loses: " + snapshot.child("1/loses").val());
+//Function that listens for winner child to be created
+database.ref().on("value", function (snapshot) {
+
+  if (snapshot.child("winner").exists()) {
+    showWinner();
+  }
+
+});
+
+//Function that listens to change in player children
+database.ref().on("value", function (snapshot) {
+
+  console.log(playersReady);
+  console.log(snapshot.child("player/1").val());
+  //Fill page with content when players 1 exist
+  if (snapshot.child("player/1").exists()) {
+
+    $("#p1-name").html(snapshot.child("player/1/name").val());
+    $("#p1-wins").html("Wins: " + snapshot.child("player/1/wins").val());
+    $("#p1-loses").html("Loses: " + snapshot.child("player/1/loses").val());
+    $("#message").html()
     console.log(snapshot.child("1/name").val());
   }
 
-  if (snapshot.child("2").exists()) {
+  //Fill page with content when players 1 exist
+  if (snapshot.child("player/2").exists()) {
 
-    $("#p2-name").html(snapshot.child("2/name").val());
-    $("#p2-wins").html("Wins: " + snapshot.child("2/wins").val());
-    $("#p2-loses").html("Loses: " + snapshot.child("2/loses").val());
+    $("#p2-name").html(snapshot.child("player/2/name").val());
+    $("#p2-wins").html("Wins: " + snapshot.child("player/2/wins").val());
+    $("#p2-loses").html("Loses: " + snapshot.child("player/2/loses").val());
     console.log(snapshot.child("2/name").val());
 
   }
 
-  if (snapshot.child("1").exists() && snapshot.child("2").exists() && playersReady === false) {
+  //Call addButtons fucntion when players exist
+  if (snapshot.child("player/1").exists() && snapshot.child("player/2").exists() && playersReady === false) {
     playersReady = true;
-    setTimeout(addButtons, 3000);
+    addButtons();
     console.log(playersReady);
+  };
+
+
+  if (playersReady == false) {
+    $("#outcome-stage").text("Waiting for players to join");
+    console.log("farts");
   }
+  else if (playersReady == true && !snapshot.child("player/1/choice").exists()) {
+    $("#outcome-stage").html("Waiting for Player 1 choice!");
+    console.log("playersReady");
+  }
+  else {
+    $("#outcome-stage").text("Waiting for Player 2 choice")
+  }
+
 
 
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
+
+
 });
 
